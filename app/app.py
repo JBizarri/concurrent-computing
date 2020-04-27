f740377908987b9fc75912efa2fec0c184709baa
JBizarri <jefferson.bizarri@gmail.com>
2020-04-27 18:47:00 -0300

    Added thread to update OilTank volume

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
-import threading
-import time
-
+from threading import Thread
 from flask import Flask, request
 from flask_restful import Api
 
-from app.modules.OilTank import OilTank
+from app.modules.OilTank.OilTank import OilTank
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,9 +12,21 @@ api.add_resource(OilTank, '/oleo')
 
 def create_app():
     global app
+    threads_classses = [OilTank]
+    
+    threads = [Thread(target=thread().run) for thread in threads_classses]
+    for thread in threads:
+        thread.start()
+        
     return app
 
 
 def create_app_local():
     global app
+    threads_classses = [OilTank]
+    
+    threads = [Thread(target=thread().run) for thread in threads_classses]
+    for thread in threads:
+        thread.start()
+    
     app.run()
